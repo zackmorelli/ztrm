7f61cb3ba5878d1e7edc9ec3ad57e5c61b048302
Zack Morelli <zackmorelli@gmail.com>
2023-02-26 20:45:36 -0500

    Home page and About me page built.

---
+++ ztrm/Pages/Books.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ztrm.Pages
+{
+    public class BooksModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
